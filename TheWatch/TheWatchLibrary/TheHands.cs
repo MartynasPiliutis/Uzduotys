df600426b1a928768072add1a2ee29ea4602214d
MartynasPiliutis <46004570+MartynasPiliutis@users.noreply.github.com>
2021-01-29 04:41:15 +0200

    TheWatchLibrary added.
    TheHands class created. Members and constructor added.

---
+++ TheWatch/TheWatchLibrary/TheHands.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheWatchLibrary
+{
+    public class TheHands
+    {
+        public int Mminute { get; private set; }
+        public int Hour { get; private set; }
+
+        public TheHands(int mminute, int hour)
+        {
+            Mminute = mminute;
+            Hour = hour;
+        }
+    }
+}
